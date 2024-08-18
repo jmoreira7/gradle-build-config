8f45757c7cc1fa615dbfec868f0cea078db230db
jmozkji <jonas.andrade7@gmail.com>
2024-08-17 21:16:16 -0300

    Create initial gradle setup.

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+// The settings file is the entry point. It contains the description of the project structure.
+
+pluginManagement {
+    repositories.gradlePluginPortal()
+//    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("gradle/plugins")
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("../my-other-project")
+}
+
+rootProject.name = "my-project"
+
+include("app")
+include("business-logic")
+include("data-model")
