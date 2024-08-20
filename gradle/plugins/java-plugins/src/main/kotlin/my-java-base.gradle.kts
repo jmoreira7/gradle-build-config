69c9bb1ffd8e2dc914fb30781c1766ee8661db82
jmozkji <jonas.andrade7@gmail.com>
2024-08-20 15:32:04 -0300

    Add important comment regarding gradle tasks and their types.

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -7,20 +7,19 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
+    /* The block 'tasks.withType<JavaCompile>().configureEach' does the same of:
+        tasks.named<JavaCompile>("compileJava") { // == tasks.compileJava {}
+            options.encoding = "UTF-8"
+        }
+
+        tasks.compileTestJava {
+            options.encoding = "UTF-8"
+        }
+    */
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
 }
 
-tasks.named<JavaCompile>("compileJava") {
-}
-
-tasks.compileTestJava {
-}
-
 tasks.test {
-
-}
-
-tasks.javadoc {
 
 }
