dc5e3c0e46a7d9526bdd951d8548e07c4fc12053
jmozkji <jonas.andrade7@gmail.com>
2024-08-20 18:54:50 -0300

    Register new gradle task.

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     id("application")
     id("my-java-base")
 }
+
+tasks.register<Zip>("bundle") {
+    group = "My group"
+    description = "packages it all!"
+
+    from(tasks.jar)
+    from(configurations.runtimeClasspath)
+
+    destinationDirectory.set(layout.buildDirectory.dir("distribution"))
+}
